a07089bcea61ab2c69f322589d62e974d13685fa
Vishal <vishal.maini@gmail.com>
2014-02-20 00:01:50 -0800

    Modified ArticleCategory model

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,5 @@
 class Category < ActiveRecord::Base
-	has_many :article_categories
-	has_many :articles, through: :article_categories
-
-	validates_uniqueness_of :name
-end
+ 	has_many :article_categories
+ 	has_many :articles, through: :article_categories
+ 	validates_uniqueness_of :name
+ end
