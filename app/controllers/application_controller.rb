c5471f23da4acb6954c04d453dfccaa8eeddef83
Vishal <vishal.maini@gmail.com>
2014-03-13 00:27:40 -0700

    Add user authentication and restrictions on creating articles

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,8 +4,18 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :get_categories
 
+  def authorize
+  	redirect_to login_url, alert: "Not authorized" if current_user.nil?
+  end
+  
   private
   def get_categories
   	@categories = Category.all.order(:name)
   end
+
+  private 
+  def current_user
+  	@current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+  helper_method :current_user
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Wikiful::Application.routes.draw do
+  get 'signup', to 'users#new', as: 'signup'
+  get 'login', to 'sessions#new', as: 'login'
+  get 'logout', to 'sessions#destroy', as: 'logout'
+  
   resources :users
+  resources :sessions
 
   get "welcome/index"
   # The priority is based upon order of creation: first created -> highest priority.
